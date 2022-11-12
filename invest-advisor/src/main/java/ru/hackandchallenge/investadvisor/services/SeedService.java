34933369fb70522cc45ce9c6ef0bf1754ae78d22
serg1996bond <serg1996bond@rambler.ru>
2022-11-13 02:01:59 +0300

    добавленна поддержка энного количества акций

--- invest-advisor/src/main/java/ru/hackandchallenge/investadvisor/services/SeedService.java
+++ invest-advisor/src/main/java/ru/hackandchallenge/investadvisor/services/SeedService.java
@@ -17,6 +17,7 @@ import ru.hackandchallenge.investadvisor.repository.OperationHistoryRepository;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -50,13 +51,23 @@ public class SeedService {
         if (!assetsRepository.findAll().isEmpty()) {
             return;
         }
-        var asset = new Asset();
-        asset.setCode("aapl");
-        asset.setFullName("APPLE");
-        asset.setCost(new BigDecimal("100"));
-        asset.setType(AssetType.STOCKS);
-        asset.setLastUpdated(LocalDateTime.now());
-        assetsRepository.save(asset);
+        var aapl = new Asset("aapl", "Apple Inc (AAPL)", new BigDecimal("100"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var ibm = new Asset("ibm", "International Business Machines (IBM)", new BigDecimal("110"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var hhr = new Asset("hhr", "HeadHunter Group PLC ADR (HHR)", new BigDecimal("120"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var hydr = new Asset("hydr", "ПАО «РусГидро» (HYDR)", new BigDecimal("130"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var ozon = new Asset("ozon", "Ozon Holdings PLC (OZON)", new BigDecimal("140"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var tatn = new Asset("tatn", "ОАО Татнефть (TATN)", new BigDecimal("150"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var chmf = new Asset("chmf", "ОАО Северсталь (CHMF)", new BigDecimal("160"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var mgnt = new Asset("mgnt", "ОАО Магнит (MGNT)", new BigDecimal("170"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var yndx = new Asset("yndx", "Яндекс Н.В. (YNDX)", new BigDecimal("180"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var aflt = new Asset("aflt", "ОАО Аэрофлот (AFLT)", new BigDecimal("190"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var afks = new Asset("afks", "ОАО АФК Система (AFKS)", new BigDecimal("200"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var rtkm = new Asset("rtkm", "Ростелеком ПАО (RTKM)", new BigDecimal("210"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var nvtk = new Asset("nvtk", "ОАО НОВАТЭК (NVTK)", new BigDecimal("220"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var pikk = new Asset("pikk", "ОАО Группа Компаний ПИК (PIKK)", new BigDecimal("230"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var nlmk = new Asset("nlmk", "Новолипецкий металлургический комбинат НЛМК (nlmk)", new BigDecimal("240"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        var dsky = new Asset("dsky", "Детский мир ПАО (DSKY)", new BigDecimal("250"), AssetType.STOCKS, LocalDateTime.now(), new HashSet<>());
+        assetsRepository.saveAll(List.of(aapl, ibm, hhr, hydr, ozon, tatn, chmf, mgnt, yndx, aflt, afks, rtkm, nvtk, pikk, nlmk, dsky));
     }
 
     private void seedInvestPortfolioAssets() {
