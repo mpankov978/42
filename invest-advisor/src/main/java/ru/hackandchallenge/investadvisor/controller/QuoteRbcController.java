c14f79353952dc1747f384cafb5186ea59d9da38
serg1996bond <serg1996bond@rambler.ru>
2022-11-13 04:24:22 +0300

    Хотфикс документации

--- invest-advisor/src/main/java/ru/hackandchallenge/investadvisor/controller/QuoteRbcController.java
+++ invest-advisor/src/main/java/ru/hackandchallenge/investadvisor/controller/QuoteRbcController.java
@@ -1,6 +1,7 @@
 package ru.hackandchallenge.investadvisor.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/collectors/dividends")
+@Tag(name = "Контроллер новостей с https://quote.rbc.ru/tag/Dividends", description = "API для получения новостей с https://quote.rbc.ru/tag/Dividends")
 public class QuoteRbcController {
 
     private final QuoteRbcCollector collector;
